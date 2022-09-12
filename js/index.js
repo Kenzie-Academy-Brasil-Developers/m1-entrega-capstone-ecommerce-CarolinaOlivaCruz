61361c3779e4c68fc8bb11653d3fe10128ea7c96
Ana Carolina Oliveira <carol.oliva.cruz@gmail.com>
2022-09-12 13:44:38 -0300

    add

--- js/index.js
+++ js/index.js
@@ -1,12 +1,43 @@
+
+function renderizaHeaderNav() {
+
+    let listaCategoria = ["Todos", "Acessórios", "Calçados", "Camisetas"];
+
+    const header = document.querySelector(".section-header");
+    const h1 = document.createElement("h1");
+    const nav = document.createElement("nav");
+    const ul = document.createElement("nav");
+
+    for (i = 0; i < listaCategoria.length; i++) {
+
+        const li = document.createElement("li");
+        li.id = "header-nav"
+        li.innerText = listaCategoria[i];
+        ul.appendChild(li);
+    }
+
+    h1.innerText = "Weartake";
+
+    nav.appendChild(ul);
+    header.append(h1, nav);
+
+}
+renderizaHeaderNav();
+
+
+
 const botoesAdd = [];
 
 function rederizaCards(array) {
 
     const ulProdutos = document.getElementById("ul-produtos");
+    ulProdutos.innerHTML = "";
 
     for (i = 0; i < array.length; i++) {
 
         const liCards = document.createElement("li");
+        const figure = document.createElement("figure");
+        const nav = document.createElement("nav");
         const img = document.createElement("img");
         const pCategoria = document.createElement("p");
         const h2 = document.createElement("h2");
@@ -17,77 +48,81 @@ function rederizaCards(array) {
         img.src = array[i].img;
         img.alt = array[i].nameItem;
 
+
         pCategoria.innerText = array[i].tag;
+        pCategoria.id = "p-categoria"
         h2.innerText = array[i].nameItem;
         pText.innerText = array[i].description;
-        h3.innerText = array[i].value;
+        h3.innerText = array[i].value.toLocaleString("pt-br", { style: "currency", currency: "BRL", });;
         buttonAdd.innerText = array[i].addCart;
         botoesAdd.push(buttonAdd)
 
-        liCards.append(img, pCategoria, h2, pText, h3, buttonAdd);
+        figure.appendChild(img);
+        nav.append(pCategoria, h2, pText, h3, buttonAdd);
+        liCards.append(figure, nav);
         ulProdutos.appendChild(liCards);
 
     }
 }
 rederizaCards(data);
 
-console.log(botoesAdd);
+// console.log(botoesAdd);
 
 
 
-
-
-function separaCategoria(array, categoria) {
+function separaCategoria(categoria) {
 
     let listaCategoria = [];
 
-    for (let i = 0; i < array.length; i++) {
-        if (array[i].tag[0] === categoria) {
-            listaCategoria.push(array[i]);
+    for (let i = 0; i < data.length; i++) {
+        if (data[i].tag[0] === categoria) {
+            listaCategoria.push(data[i]);
         }
     }
 
     return listaCategoria;
 }
-console.log(separaCategoria(data, "Camisetas"));
-console.log(separaCategoria(data, "Acessórios"));
-console.log(separaCategoria(data, "Calçados"));
+
+console.log(separaCategoria("Camisetas"));
+console.log(separaCategoria("Acessórios"));
+console.log(separaCategoria("Calçados"));
+
+header-nav.addEventListener("click", separaCategoria);
 
 
-function buscarCategoria() {
-    console.log("Clicou");
+
+
+function renderizaCampoBusca() {
+    const campoBusca = document.querySelector(".busca");
+    console.log(campoBusca)
+    const inputBusca = document.createElement("input");
+    inputBusca.type = "text";
+    inputBusca.placeholder = "Digite aqui sua pesquisa";
+
+    const buttonBusca = document.createElement("button");
+    buttonBusca.innerText = "Pesquisar";
+    buttonBusca.addEventListener("click", buscarProduto);
+
+    campoBusca.append(inputBusca, buttonBusca);
+
 }
+renderizaCampoBusca();
 
 
+function buscarProduto() {
 
+    let inputBusca = document.querySelector("input");
+    let saida = [];
 
-// function renderizaCampoBusca() {
+    for (let i = 0; i < data.length; i++) {
 
-//     const campoBusca = document.getElementsByClassName("busca");
+        if (inputBusca.value === data[i].nameItem) {
+            saida.push(data[i]);
+        }
+    }
 
-//     const inputBusca = document.createElement("input");
-//     inputBusca.type = "text";
-//     inputBusca.placeholder = "Digite aqui sua pesquisa";
-
-//     // if (inputBusca.placeholder === "Camisetas") {
-//     //     return separaCategoria(data, "Camisetas");
-//     // } else if (inputBusca.placeholder === "Acessórios") {
-//     //     return separaCategoria(data, "Acessórios");
-//     // } else if (inputBusca.placeholder === "Calçados") {
-//     //     return separaCategoria(data, "Calçados");
-//     // }
-
-//     const buttonBusca = document.createElement("button");
-//     buttonBusca.innerText = "Buscar";
-
-//     // buttonBusca.addEventListener("click", buscar);
-
-//     campoBusca.append(inputBusca, buttonBusca);
-
-// }
-// renderizaCampoBusca();
-
-
+    rederizaCards(saida);
+}
 
 
 
